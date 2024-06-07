7003f048d9667c907a1073d52930d9c0c0a0489c
Lauro Caetano <laurocaetano1@gmail.com>
2024-06-08 00:20:56 +0200

    Rework types

--- raft/raft.go
+++ raft/raft.go
@@ -1,17 +1,29 @@
 package raft
 
+type (
+	State    uint
+	ServerId string
+)
+
+const (
+	Follower State = iota
+	Candidate
+	Leader
+)
+
 type Log struct {
 	Index uint64
 	Term  uint64
 	Data  []byte
 }
 
-type ServerId string
-
 type Server struct {
-	Id          ServerId
-	Address     string
-	CurrentTerm uint64
-	VotedFor    ServerId
-	Log         []Log
+	id      ServerId
+	address string
+}
+
+type RaftState struct {
+	currentTerm uint64
+	log         []Log
+	peers       []Server
 }
--- raft/rpc.go
+++ raft/rpc.go
@@ -1,19 +1,17 @@
 package raft
 
 type RequestVoteRequest struct {
-	Term         uint64
-	CandidateId  ServerId
-	LastLogIndex uint64
-	LastLogTerm  uint64
+	Term         uint64   `json:"term,omitempty"`
+	CandidateId  ServerId `json:"candidate_id,omitempty"`
+	LastLogIndex uint64   `json:"last_log_index,omitempty"`
+	LastLogTerm  uint64   `json:"last_log_term,omitempty"`
 }
 
 type RequestVoteResponse struct {
-	Term        uint64
-	VoteGranted bool
+	Term        uint64 `json:"term,omitempty"`
+	VoteGranted bool   `json:"vote_granted,omitempty"`
 }
 
 type RequestVoteRpc interface {
 	RequestVote(request RequestVoteRequest) (RequestVoteResponse, error)
 }
-
-type TcpClient struct{}
